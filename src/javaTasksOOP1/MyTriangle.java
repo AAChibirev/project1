ebcd23ae9665c8c2aafbf970b731e88bf4f84a17
AAChibirev <81084076+AAChibirev@users.noreply.github.com>
2021-04-07 15:46:11 +0300

    Update MyTriangle.java
    correct compare double in getType() with BigDecimal

--- src/javaTasksOOP1/MyTriangle.java
+++ src/javaTasksOOP1/MyTriangle.java
@@ -1,5 +1,7 @@
 package javaTasksOOP1;
 
+import java.math.BigDecimal;
+
 class MyTriangle {
     private MyPoint v1;
     private MyPoint v2;
@@ -31,19 +33,18 @@ class MyTriangle {
     }
 
     public Enum getType(){
-        double l1, l2, l3;
-        l1 = v1.distance(v2);
-        l2 = v2.distance(v3);
-        l3 = v1.distance(v3);
+        BigDecimal l1 = new BigDecimal(v1.distance(v2));
+        BigDecimal l2 = new BigDecimal(v2.distance(v3));
+        BigDecimal l3 = new BigDecimal(v1.distance(v3));
 
-        if(l1 == l2){
-            if(l1 == l3){
+        if(l1.compareTo(l2) == 0){
+            if(l1.compareTo(l3) == 0){
                 return  TriangleType.EQUILATERAL;
             } else return TriangleType.ISOSCELES;
         }
-        if(l1 == l3){
+        if(l1.compareTo(l3) == 0){
             return TriangleType.ISOSCELES;
-        } else if (l2 == l3){
+        } else if (l2.compareTo(l3) == 0){
             return TriangleType.ISOSCELES;
         } else return TriangleType.SCALENE;
 
